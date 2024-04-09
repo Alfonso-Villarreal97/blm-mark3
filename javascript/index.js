e8e97e60893f4cac4eef46d7d99f864ff68f6df6
EstebanSGJ <garciaestebansantiago11@gmail.com>
2024-04-09 15:21:39 -0600

    Implementacion Seguimeinto

--- javascript/index.js
+++ javascript/index.js
@@ -9,6 +9,7 @@ var opcionreportelaboral = document.querySelectorAll(".reporte-lab-icono");
 var opcionconsultas = document.querySelectorAll(".consultas-icono");
 var opcionbuscarempleo = document.querySelector(".buscar_empleo_boton");
 var opcionbolsatrabajoicono = document.querySelectorAll(".bolsatrabajoicono");
+var opcionseguimiento=document.querySelectorAll(".boton_seguimiento");
 
 
 
@@ -21,6 +22,7 @@ var fondoconsulta = document.querySelector(".fondoconsulta");/*6*/
 var fondobuscarempleo = document.querySelector(".fondobuscartrabajo"); /*7*/
 var fondocontactanos = document.querySelector(".fondocontactanos"); /* 8 */
 var fondoblog=document.querySelector(".fondoblog");/* 9 */
+var fondoseguimiento=document.querySelector(".fondoseguimiento") /* 10 */
 
 
 
@@ -33,6 +35,7 @@ var contenidoconsultas = document.querySelector(".contenido-consultas");/*6*/
 var contenidobuscartrabajo = document.querySelector(".contenido_buscar_empleo"); /* 7 */
 var contenidocontactanos = document.querySelector(".contenido_contactanos"); /* 8 */
 var contenido_blog=document.querySelector(".contenido_blog");/* 9 */
+var contenidoseguimiento=document.querySelector(".contenido_seguimiento") /* 10 */
 
 var colorletrasmenu = document.querySelectorAll(".opc");
 
@@ -67,8 +70,8 @@ function menudiseno(elemento1, elemento2, elemento3, elemento4, elemento5) {
 
 OpcionInicio.addEventListener("click", function () {
     menudiseno(OpcionQuienessomos, OpcionServicios, OpcionContactanos, OpcionBlog, OpcionInicio);
-    fondo(fondoservicios, fondocontactanos, fondoquienessomos, fondoconsulta, fondoreportelab, fondobolsadetrabajo, fondobuscarempleo, fondoblog,fondovideoprincipal);
-    contenido(contenidoconsultas, contenidocontactanos, contenidobuscartrabajo, contenidoquienessomos, contenidobolsadetrabajo, contenidoservicios, contenidoreportelaboral, contenido_blog,contenidoprincipal);
+    fondo(fondoservicios, fondoseguimiento,fondocontactanos, fondoquienessomos, fondoconsulta, fondoreportelab, fondobolsadetrabajo, fondobuscarempleo, fondoblog,fondovideoprincipal);
+    contenido(contenidoconsultas, contenidoseguimiento,contenidocontactanos, contenidobuscartrabajo, contenidoquienessomos, contenidobolsadetrabajo, contenidoservicios, contenidoreportelaboral, contenido_blog,contenidoprincipal);
     for (var i = 0; i < colorletrasmenu.length; i++) {
         colorletrasmenu[i].style.color = "white";
     }
@@ -76,8 +79,8 @@ OpcionInicio.addEventListener("click", function () {
 
 OpcionQuienessomos.addEventListener("click", function () {
     menudiseno(OpcionInicio, OpcionServicios, OpcionContactanos, OpcionBlog, OpcionQuienessomos);
-    fondo(fondoservicios, fondocontactanos, fondoconsulta, fondobuscarempleo, fondoreportelab, fondovideoprincipal, fondobolsadetrabajo,fondoblog, fondoquienessomos);
-    contenido(contenidoconsultas, contenidocontactanos, contenidobuscartrabajo, contenidobolsadetrabajo, contenidoservicios, contenidoprincipal, contenidoreportelaboral, contenido_blog,contenidoquienessomos);
+    fondo(fondoservicios, fondoseguimiento,fondocontactanos, fondoconsulta, fondobuscarempleo, fondoreportelab, fondovideoprincipal, fondobolsadetrabajo,fondoblog, fondoquienessomos);
+    contenido(contenidoconsultas, contenidoseguimiento,contenidocontactanos, contenidobuscartrabajo, contenidobolsadetrabajo, contenidoservicios, contenidoprincipal, contenidoreportelaboral, contenido_blog,contenidoquienessomos);
     for (var i = 0; i < colorletrasmenu.length; i++) {
         colorletrasmenu[i].style.color = "black";
     }
@@ -86,8 +89,8 @@ OpcionQuienessomos.addEventListener("click", function () {
 
 OpcionServicios.addEventListener("click", function () {
     menudiseno(OpcionInicio, OpcionContactanos, OpcionBlog, OpcionQuienessomos, OpcionServicios);
-    fondo(fondovideoprincipal, fondocontactanos, fondobuscarempleo, fondoconsulta, fondoreportelab, fondobolsadetrabajo, fondoquienessomos, fondoblog,fondoservicios);
-    contenido(contenidoprincipal, contenidocontactanos, contenidobuscartrabajo, contenidobolsadetrabajo, contenidoquienessomos, contenidoconsultas, contenidoreportelaboral, contenido_blog,contenidoservicios);
+    fondo(fondovideoprincipal, fondoseguimiento,fondocontactanos, fondobuscarempleo, fondoconsulta, fondoreportelab, fondobolsadetrabajo, fondoquienessomos, fondoblog,fondoservicios);
+    contenido(contenidoprincipal, contenidoseguimiento,contenidocontactanos, contenidobuscartrabajo, contenidobolsadetrabajo, contenidoquienessomos, contenidoconsultas, contenidoreportelaboral, contenido_blog,contenidoservicios);
     contenidoservicios.style.display = "flex";
     for (var i = 0; i < colorletrasmenu.length; i++) {
         colorletrasmenu[i].style.color = "black";
@@ -103,16 +106,16 @@ OpcionBlog.addEventListener("click", function () {
 
 OpcionContactanos.addEventListener("click", function () {
     menudiseno(OpcionQuienessomos, OpcionServicios, OpcionBlog, OpcionInicio, OpcionContactanos);
-    contenido(contenidoprincipal, contenidoreportelaboral, contenidoquienessomos, contenidoservicios, contenidobolsadetrabajo, contenidoconsultas, contenidobuscartrabajo, contenido_blog,contenidocontactanos);
-    fondo(fondoservicios, fondoquienessomos, fondoservicios, fondovideoprincipal, fondobolsadetrabajo, fondoconsulta, fondobuscarempleo, fondoblog,fondocontactanos);
+    contenido(contenidoprincipal, contenidoseguimiento,contenidoreportelaboral, contenidoquienessomos, contenidoservicios, contenidobolsadetrabajo, contenidoconsultas, contenidobuscartrabajo, contenido_blog,contenidocontactanos);
+    fondo(fondoservicios, fondoseguimiento,fondoquienessomos, fondoservicios, fondovideoprincipal, fondobolsadetrabajo, fondoconsulta, fondobuscarempleo, fondoblog,fondocontactanos);
     for (var i = 0; i < colorletrasmenu.length; i++) {
         colorletrasmenu[i].style.color = "black ";
     }
 });
 OpcionBlog.addEventListener("click", function () {
     menudiseno(OpcionQuienessomos, OpcionServicios, OpcionInicio, OpcionContactanos,OpcionBlog);
-    contenido(contenidoprincipal, contenidoreportelaboral, contenidoquienessomos, contenidoservicios, contenidobolsadetrabajo, contenidoconsultas, contenidobuscartrabajo,contenidocontactanos, contenido_blog);
-    fondo(fondoservicios, fondoquienessomos, fondoservicios, fondovideoprincipal, fondobolsadetrabajo, fondoconsulta, fondobuscarempleo,fondocontactanos, fondoblog);
+    contenido(contenidoprincipal, contenidoseguimiento,contenidoreportelaboral, contenidoquienessomos, contenidoservicios, contenidobolsadetrabajo, contenidoconsultas, contenidobuscartrabajo,contenidocontactanos, contenido_blog);
+    fondo(fondoservicios, fondoseguimiento,fondoquienessomos, fondoservicios, fondovideoprincipal, fondobolsadetrabajo, fondoconsulta, fondobuscarempleo,fondocontactanos, fondoblog);
     for (var i = 0; i < colorletrasmenu.length; i++) {
         colorletrasmenu[i].style.color = "black ";
     }
@@ -123,42 +126,53 @@ OpcionBlog.addEventListener("click", function () {
 
 
 opcionreportelaboral[0].addEventListener("click", function () {
-    fondo(fondoservicios, fondoquienessomos, fondocontactanos, fondoconsulta, fondobuscarempleo, fondovideoprincipal, fondobolsadetrabajo, fondoblog,fondoreportelab);
-    contenido(contenidoprincipal, contenidocontactanos, contenidobuscartrabajo, contenidoquienessomos, contenidoservicios, contenidobolsadetrabajo, contenidoconsultas, contenido_blog,contenidoreportelaboral);
+    fondo(fondoservicios, fondoseguimiento,fondoquienessomos, fondocontactanos, fondoconsulta, fondobuscarempleo, fondovideoprincipal, fondobolsadetrabajo, fondoblog,fondoreportelab);
+    contenido(contenidoprincipal,contenidoseguimiento ,contenidocontactanos, contenidobuscartrabajo, contenidoquienessomos, contenidoservicios, contenidobolsadetrabajo, contenidoconsultas, contenido_blog,contenidoreportelaboral);
 })
 
 opcionreportelaboral[1].addEventListener("click", function () {
-    fondo(fondoservicios, fondoquienessomos, fondocontactanos, fondoconsulta, fondobuscarempleo, fondovideoprincipal, fondobolsadetrabajo, fondoblog,fondoreportelab);
-    contenido(contenidoprincipal, contenidocontactanos, contenidobuscartrabajo, contenidoquienessomos, contenidoservicios, contenidobolsadetrabajo, contenidoconsultas, contenido_blog,contenidoreportelaboral);
+    fondo(fondoservicios, fondoseguimiento,fondoquienessomos, fondocontactanos, fondoconsulta, fondobuscarempleo, fondovideoprincipal, fondobolsadetrabajo, fondoblog,fondoreportelab);
+    contenido(contenidoprincipal, contenidoseguimiento,contenidocontactanos, contenidobuscartrabajo, contenidoquienessomos, contenidoservicios, contenidobolsadetrabajo, contenidoconsultas, contenido_blog,contenidoreportelaboral);
 })
 
 opcionconsultas[0].addEventListener("click", function () {
-    contenido(contenidoprincipal, contenidobuscartrabajo, contenidoquienessomos, contenidoservicios, contenidoreportelaboral, contenidobolsadetrabajo, contenido_blog,contenidoconsultas);
-    fondo(fondoservicios, contenidocontactanos, fondocontactanos, fondoquienessomos, fondobuscarempleo, fondovideoprincipal, fondoreportelab, fondobolsadetrabajo, fondoblog,fondoconsulta);
+    contenido(contenidoprincipal, contenidoseguimiento,contenidobuscartrabajo, contenidoquienessomos, contenidoservicios, contenidoreportelaboral, contenidobolsadetrabajo, contenido_blog,contenidoconsultas);
+    fondo(fondoservicios, fondoseguimiento, fondocontactanos, fondoquienessomos, fondobuscarempleo, fondovideoprincipal, fondoreportelab, fondobolsadetrabajo, fondoblog,fondoconsulta);
 
 })
 
 opcionconsultas[1].addEventListener("click", function () {
-    contenido(contenidoprincipal, contenidocontactanos, contenidobuscartrabajo, contenidoquienessomos, contenidoservicios, contenidoreportelaboral, contenidobolsadetrabajo, contenido_blog,contenidoconsultas);
-    fondo(fondoservicios, fondocontactanos, fondoquienessomos, fondobuscarempleo, fondovideoprincipal, fondoreportelab, fondobolsadetrabajo, fondoblog,fondoconsulta);
+    contenido(contenidoprincipal, contenidoseguimiento,contenidocontactanos, contenidobuscartrabajo, contenidoquienessomos, contenidoservicios, contenidoreportelaboral, contenidobolsadetrabajo, contenido_blog,contenidoconsultas);
+    fondo(fondoservicios, fondoseguimiento,fondocontactanos, fondoquienessomos, fondobuscarempleo, fondovideoprincipal, fondoreportelab, fondobolsadetrabajo, fondoblog,fondoconsulta);
 
 })
 
 opcionbuscarempleo.addEventListener("click", function () {
     menudiseno(OpcionQuienessomos, OpcionServicios, OpcionContactanos, OpcionBlog, OpcionInicio);
-    contenido(contenidoprincipal, contenidocontactanos, contenidoreportelaboral, contenidoquienessomos, contenidoservicios, contenidobolsadetrabajo, contenidoconsultas, contenido_blog,contenidobuscartrabajo);
-    fondo(fondoservicios, fondoquienessomos, fondoservicios, fondovideoprincipal, fondobolsadetrabajo, fondoconsulta, fondocontactanos, fondoblog,fondobuscarempleo);
+    contenido(contenidoprincipal, contenidoseguimiento,contenidocontactanos, contenidoreportelaboral, contenidoquienessomos, contenidoservicios, contenidobolsadetrabajo, contenidoconsultas, contenido_blog,contenidobuscartrabajo);
+    fondo(fondoservicios, fondoseguimiento,fondoquienessomos, fondoservicios, fondovideoprincipal, fondobolsadetrabajo, fondoconsulta, fondocontactanos, fondoblog,fondobuscarempleo);
     for (var i = 0; i < colorletrasmenu.length; i++) {
         colorletrasmenu[i].style.color = "black ";
     }
 })
 
 opcionbolsatrabajoicono[0].addEventListener("click",function(){
-    contenido(contenidoservicios,contenidoreportelaboral,contenidoquienessomos,contenidoprincipal,contenidocontactanos,contenidoconsultas,contenidobuscartrabajo,contenido_blog,contenidobolsadetrabajo);
-    fondo(fondovideoprincipal,fondoservicios,fondoreportelab,fondoquienessomos,fondocontactanos,fondoconsulta,fondobuscarempleo,fondoblog,fondobolsadetrabajo);
+    contenido(contenidoservicios,contenidoseguimiento,contenidoreportelaboral,contenidoquienessomos,contenidoprincipal,contenidocontactanos,contenidoconsultas,contenidobuscartrabajo,contenido_blog,contenidobolsadetrabajo);
+    fondo(fondovideoprincipal,fondoseguimiento,fondoservicios,fondoreportelab,fondoquienessomos,fondocontactanos,fondoconsulta,fondobuscarempleo,fondoblog,fondobolsadetrabajo);
 })
 
 opcionbolsatrabajoicono[1].addEventListener("click",function(){
-    contenido(contenidoservicios,contenidoreportelaboral,contenidoquienessomos,contenidoprincipal,contenidocontactanos,contenidoconsultas,contenidobuscartrabajo,contenido_blog,contenidobolsadetrabajo);
-    fondo(fondovideoprincipal,fondoservicios,fondoreportelab,fondoquienessomos,fondocontactanos,fondoconsulta,fondobuscarempleo,fondoblog,fondobolsadetrabajo);
+    contenido(contenidoservicios,contenidoseguimiento,contenidoreportelaboral,contenidoquienessomos,contenidoprincipal,contenidocontactanos,contenidoconsultas,contenidobuscartrabajo,contenido_blog,contenidobolsadetrabajo);
+    fondo(fondovideoprincipal,fondoseguimiento,fondoservicios,fondoreportelab,fondoquienessomos,fondocontactanos,fondoconsulta,fondobuscarempleo,fondoblog,fondobolsadetrabajo);
+})
+
+/* ''''' */
+opcionseguimiento[0].addEventListener("click",function(){
+    contenido(contenidoservicios,contenidoreportelaboral,contenidoquienessomos,contenidoprincipal,contenidocontactanos,contenidoconsultas,contenidobuscartrabajo,contenido_blog,contenidobolsadetrabajo,contenidoseguimiento);
+    fondo(fondovideoprincipal,fondoservicios,fondoreportelab,fondoquienessomos,fondocontactanos,fondoconsulta,fondobuscarempleo,fondoblog,fondobolsadetrabajo,fondoseguimiento);
+})
+
+opcionseguimiento[1].addEventListener("click",function(){
+    contenido(contenidoservicios,contenidoreportelaboral,contenidoquienessomos,contenidoprincipal,contenidocontactanos,contenidoconsultas,contenidobuscartrabajo,contenido_blog,contenidobolsadetrabajo,contenidoseguimiento,contenidoseguimiento);
+    fondo(fondovideoprincipal,fondoservicios,fondoreportelab,fondoquienessomos,fondocontactanos,fondoconsulta,fondobuscarempleo,fondoblog,fondobolsadetrabajo,fondoseguimiento);
 })
